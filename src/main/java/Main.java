3ff080165cfd5bef54a6d80c8f3f9da957b690df
Kevinrosales <kevinrosalesolvera@gmail.com>
2018-11-27 13:28:14 -0800

    got the first two working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        String myString = "cat";
+        String word = pluralize(myString, 0);
+        System.out.println(word);
+        flipNHeads(2);
+    }
+    public static String pluralize(String animal, int num) {
+        if (num == 0 || num > 1) {
+            animal = animal + "s";
+        }
+        return animal;
+    }
+    public static void flipNHeads(int n) {
+        int head = 0;
+        int timesFlipped;
+        for (timesFlipped = 0; head < n; timesFlipped++) {
+            double rando = Math.random();
+            if (rando < 0.5) {
+                System.out.println("Tails");
+            } else {
+                System.out.println("Heads");
+                head++;
+            }
+        }
+        System.out.println("It took " + timesFlipped + " flips to flip " + n + " heads in a row.");
+
     }
 }
