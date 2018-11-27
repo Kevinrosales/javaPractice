2d4ac0cdffe7458170c606b6cc6f744d4a52b15b
Kevinrosales <kevinrosalesolvera@gmail.com>
2018-11-27 15:13:27 -0800

    did better on the first one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        String myString = "cat";
-        String word = pluralize(myString, 0);
-        System.out.println(word);
+
+        int dogCount = 1;
+        System.out.println("I own " + dogCount + " " + pluralize("dog", dogCount) + ".");
+
+        int catCount = 2;
+        System.out.println("I own " + catCount + " " + pluralize("cat", catCount) + ".");
+
+        int turtleCount = 0;
+        System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
+
         flipNHeads(2);
     }
     public static String pluralize(String animal, int num) {
